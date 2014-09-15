b064d55f528b5607505bcc8fd2a63648afb2158e
Bernard Lambeau <blambeau@gmail.com>
2014-09-15 13:57:22 +0200

    Fix spacing, avoid redundant test assertions.

--- src/test/java/jalf/JAlfDSLTest.java
+++ src/test/java/jalf/JAlfDSLTest.java
@@ -24,32 +24,30 @@ public class JAlfDSLTest {
     @Test
     public void testProject() {
         Relation expected = relation(
-                tuple(SID, "S1", NAME, "Smith"),
-                tuple(SID, "S2", NAME, "Jones"),
-                tuple(SID, "S3", NAME, "Blake"),
-                tuple(SID, "S4", NAME, "Clark"),
-                tuple(SID, "S5", NAME, "Adams")
+            tuple(SID, "S1", NAME, "Smith"),
+            tuple(SID, "S2", NAME, "Jones"),
+            tuple(SID, "S3", NAME, "Blake"),
+            tuple(SID, "S4", NAME, "Clark"),
+            tuple(SID, "S5", NAME, "Adams")
         );
 
         Relation actual = project(suppliers(), attrs(SID, NAME));
 
-        assertEquals(5, actual.count());
         assertEquals(expected, actual);
     }
 
     @Test
     public void testRename() {
         Relation expected = relation(
-                tuple(SUPPLIER_ID, "S1", NAME, "Smith", STATUS, 20, LIVES_IN, "London"),
-                tuple(SUPPLIER_ID, "S2", NAME, "Jones", STATUS, 10, LIVES_IN, "Paris"),
-                tuple(SUPPLIER_ID, "S3", NAME, "Blake", STATUS, 30, LIVES_IN, "Paris"),
-                tuple(SUPPLIER_ID, "S4", NAME, "Clark", STATUS, 20, LIVES_IN, "London"),
-                tuple(SUPPLIER_ID, "S5", NAME, "Adams", STATUS, 30, LIVES_IN, "Athens")
+            tuple(SUPPLIER_ID, "S1", NAME, "Smith", STATUS, 20, LIVES_IN, "London"),
+            tuple(SUPPLIER_ID, "S2", NAME, "Jones", STATUS, 10, LIVES_IN, "Paris"),
+            tuple(SUPPLIER_ID, "S3", NAME, "Blake", STATUS, 30, LIVES_IN, "Paris"),
+            tuple(SUPPLIER_ID, "S4", NAME, "Clark", STATUS, 20, LIVES_IN, "London"),
+            tuple(SUPPLIER_ID, "S5", NAME, "Adams", STATUS, 30, LIVES_IN, "Athens")
         );
 
         Relation actual = rename(suppliers(), renaming(SID, SUPPLIER_ID, CITY, LIVES_IN));
 
-        assertEquals(5, actual.count());
         assertEquals(expected, actual);
     }
 
--- src/test/java/jalf/test/fixtures/SuppliersAndParts.java
+++ src/test/java/jalf/test/fixtures/SuppliersAndParts.java
@@ -1,14 +1,10 @@
 package jalf.test.fixtures;
 
-import jalf.Relation;
-
 import static jalf.JAlfDSL.relation;
 import static jalf.JAlfDSL.tuple;
+import jalf.Relation;
 
 public class SuppliersAndParts {
-
-    public static final String SUPPLIER_ID = "supplier_id";
-    public static final String LIVES_IN = "lives_in";
 
     public static final String SID = "sid";
     public static final String NAME = "name";
@@ -21,41 +17,44 @@ public class SuppliersAndParts {
 
     public static final String QTY = "qty";
 
+    public static final String SUPPLIER_ID = "supplier_id";
+    public static final String LIVES_IN = "lives_in";
+
     public static Relation suppliers() {
         return relation(
-                tuple(SID, "S1", NAME, "Smith", STATUS, 20, CITY, "London"),
-                tuple(SID, "S2", NAME, "Jones", STATUS, 10, CITY, "Paris"),
-                tuple(SID, "S3", NAME, "Blake", STATUS, 30, CITY, "Paris"),
-                tuple(SID, "S4", NAME, "Clark", STATUS, 20, CITY, "London"),
-                tuple(SID, "S5", NAME, "Adams", STATUS, 30, CITY, "Athens")
+            tuple(SID, "S1", NAME, "Smith", STATUS, 20, CITY, "London"),
+            tuple(SID, "S2", NAME, "Jones", STATUS, 10, CITY, "Paris"),
+            tuple(SID, "S3", NAME, "Blake", STATUS, 30, CITY, "Paris"),
+            tuple(SID, "S4", NAME, "Clark", STATUS, 20, CITY, "London"),
+            tuple(SID, "S5", NAME, "Adams", STATUS, 30, CITY, "Athens")
         );
     }
 
     public static Relation parts() {
         return relation(
-                tuple(PID, "P1", NAME, "Nut", COLOR, "Red", WEIGHT, 12.0, CITY, "London"),
-                tuple(PID, "P2", NAME, "Bolt", COLOR, "Green", WEIGHT, 17.0, CITY, "Paris"),
-                tuple(PID, "P3", NAME, "Screw", COLOR, "Blue", WEIGHT, 17.0, CITY, "Oslo"),
-                tuple(PID, "P4", NAME, "Screw", COLOR, "Red", WEIGHT, 14.0, CITY, "London"),
-                tuple(PID, "P5", NAME, "Cam", COLOR, "Blue", WEIGHT, 12.0, CITY, "Paris"),
-                tuple(PID, "P6", NAME, "Cog", COLOR, "Red", WEIGHT, 19.0, CITY, "London")
+            tuple(PID, "P1", NAME, "Nut",   COLOR, "Red",   WEIGHT, 12.0, CITY, "London"),
+            tuple(PID, "P2", NAME, "Bolt",  COLOR, "Green", WEIGHT, 17.0, CITY, "Paris"),
+            tuple(PID, "P3", NAME, "Screw", COLOR, "Blue",  WEIGHT, 17.0, CITY, "Oslo"),
+            tuple(PID, "P4", NAME, "Screw", COLOR, "Red",   WEIGHT, 14.0, CITY, "London"),
+            tuple(PID, "P5", NAME, "Cam",   COLOR, "Blue",  WEIGHT, 12.0, CITY, "Paris"),
+            tuple(PID, "P6", NAME, "Cog",   COLOR, "Red",   WEIGHT, 19.0, CITY, "London")
         );
     }
 
     public static Relation shipments() {
         return relation(
-                tuple(SID, "S1", PID, "P1", QTY, 300),
-                tuple(SID, "S1", PID, "P2", QTY, 200),
-                tuple(SID, "S1", PID, "P3", QTY, 400),
-                tuple(SID, "S1", PID, "P4", QTY, 200),
-                tuple(SID, "S1", PID, "P5", QTY, 100),
-                tuple(SID, "S1", PID, "P6", QTY, 100),
-                tuple(SID, "S2", PID, "P1", QTY, 300),
-                tuple(SID, "S2", PID, "P2", QTY, 400),
-                tuple(SID, "S3", PID, "P2", QTY, 200),
-                tuple(SID, "S4", PID, "P2", QTY, 200),
-                tuple(SID, "S4", PID, "P4", QTY, 300),
-                tuple(SID, "S4", PID, "P5", QTY, 400)
+            tuple(SID, "S1", PID, "P1", QTY, 300),
+            tuple(SID, "S1", PID, "P2", QTY, 200),
+            tuple(SID, "S1", PID, "P3", QTY, 400),
+            tuple(SID, "S1", PID, "P4", QTY, 200),
+            tuple(SID, "S1", PID, "P5", QTY, 100),
+            tuple(SID, "S1", PID, "P6", QTY, 100),
+            tuple(SID, "S2", PID, "P1", QTY, 300),
+            tuple(SID, "S2", PID, "P2", QTY, 400),
+            tuple(SID, "S3", PID, "P2", QTY, 200),
+            tuple(SID, "S4", PID, "P2", QTY, 200),
+            tuple(SID, "S4", PID, "P4", QTY, 300),
+            tuple(SID, "S4", PID, "P5", QTY, 400)
         );
     }
 
