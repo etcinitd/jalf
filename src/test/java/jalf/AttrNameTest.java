1dfdb4e8e88ee13759bf3fe729708512f0abeafd
Bernard Lambeau <blambeau@gmail.com>
2014-09-25 08:50:37 +0200

    Add unit tests for AttrName (refs #4)

---
+++ src/test/java/jalf/AttrNameTest.java
@@ -0,0 +1,36 @@
+package jalf;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AttrNameTest {
+
+    AttrName RID = AttrName.attr("rid");
+    AttrName SID = AttrName.attr("sid");
+    AttrName WID = AttrName.attr("wid");
+
+    @Test
+    public void testGetName() {
+        assertEquals("sid", SID.getName());
+    }
+
+    @Test
+    public void testItIsComparableByName() {
+        assertEquals(0, SID.compareTo(SID));
+        assertTrue(SID.compareTo(WID) < 0);
+        assertTrue(SID.compareTo(RID) > 0);
+    }
+
+    @Test
+    public void testEquals(){
+        assertEquals(SID, AttrName.attr("sid"));
+        assertNotEquals(SID, WID);
+    }
+
+    @Test
+    public void testHashCode(){
+        assertEquals(SID.hashCode(), AttrName.attr("sid").hashCode());
+    }
+
+}
