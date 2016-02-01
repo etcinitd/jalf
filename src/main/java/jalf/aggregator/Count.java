5bd61b21d4db409a66047be7c3b1801e968f4ba0
anne-france <anne-france@excite.com>
2016-02-01 05:59:53 +0100

    structure

---
+++ src/main/java/jalf/aggregator/Count.java
@@ -0,0 +1,13 @@
+package jalf.aggregator;
+
+import jalf.AttrName;
+
+public class Count extends Aggregator{
+    public Count(AttrName attr){
+        this.nameaggr="count";
+        this.attr=attr;
+
+    }
+
+
+}
---
+++ src/main/java/jalf/aggregator/Max.java
@@ -0,0 +1,11 @@
+package jalf.aggregator;
+
+import jalf.AttrName;
+
+public class Max extends Aggregator{
+    public Max(AttrName attr){
+        this.nameaggr="max";
+        this.attr=attr;
+
+    }
+}
