4007a07198cbbf1b61be7d1b6e91e39aebe675f8
aimablehirwa <aimable.hirwa@student.uclouvain.be>
2016-02-28 00:25:06 +0100

    test optimization summarize

--- src/test/java/jalf/optimizer/TestOptimizedSummarize.java
+++ src/test/java/jalf/optimizer/TestOptimizedSummarize.java
@@ -2,67 +2,88 @@ package jalf.optimizer;
 
 import static jalf.DSL.attr;
 import static jalf.DSL.attrs;
-import static jalf.DSL.avg;
 import static jalf.DSL.count;
 import static jalf.DSL.eq;
 import static jalf.DSL.max;
-import static jalf.DSL.project;
 import static jalf.DSL.summarize;
+import static jalf.fixtures.SuppliersAndParts.NAME;
+import static jalf.fixtures.SuppliersAndParts.PID;
 import static jalf.fixtures.SuppliersAndParts.QTY;
 import static jalf.fixtures.SuppliersAndParts.SID;
 import static jalf.fixtures.SuppliersAndParts.shipments;
 import jalf.Relation;
+import jalf.TypeException;
 
 import org.junit.Test;
 
-public class TestOptimizedSummarize {
+public class TestOptimizedSummarize  extends OptimizerTest {
 
     private Relation rel = shipments();
 
+    // projet optimization
     @Test
     public void testProject() {
 
-        Relation operator = summarize(rel,attrs(SID),count(),attr("count"));
-        Relation optimized = optimized(operator).
-                project(attrs(SID));
-
-        Relation expected = summarize(
-                project(rel, attrs(SID)),
-                attrs(SID),
-                count(),
-                attr("count"));
-
+        // normal execution
+        Relation operator = summarize(rel,attrs(SID, PID),count(),attr("count"));
+        Relation optimized = optimized(operator).project(attrs(SID, attr("count")));
+        Relation expected = rel.summarize(attrs(SID), count(), attr("count"));
         assertSameExpression(expected, optimized);
 
-        operator = summarize(rel,attrs(SID), avg(QTY),attr("avg"));
-        optimized = optimized(operator).project(attrs(SID, QTY));
+    }
 
-        expected = rel.
-                project(attrs(SID, QTY)).
-                summarize(attrs(SID), avg(QTY), attr("ag"));
+    @Test(expected=TypeException.class)
+    public void testProject1() {
 
-        assertSameExpression(expected, optimized);
+        // there is an intersect between by and on but on is not included in by
+        Relation operator = summarize(rel,attrs(SID, PID), max(QTY),attr("count"));
+        optimized(operator).project(attrs(NAME, attr("count")));
+
+    }
+
+    @Test(expected=TypeException.class)
+    public void testProject2() {
+
+        // by and on are the same
+        Relation operator = summarize(rel,attrs(SID, PID),count(),attr("count"));
+        optimized(operator).project(attrs(NAME, QTY));
+
     }
 
     @Test
+    public void testProject3() {
+
+        // by and on are the same
+        Relation operator = summarize(rel,attrs(SID, PID),count(),attr("count"));
+        Relation optimized = optimized(operator).project(attrs(SID, PID, attr("count")));
+        Relation expected = rel.summarize(attrs(SID, PID), count(), attr("count"));
+        assertSameExpression(expected, optimized);
+
+    }
+
+    // restrict optimization
+
+    @Test
     public void testRestrict(){
-        Relation operator =  summarize(rel,attrs(SID), count(),attr("count"));;
+
+        // normal execution
+        Relation operator =  summarize(rel,attrs(SID, PID), count(), attr("count"));
         Relation optimized = optimized(operator).restrict(eq(SID, "S1"));
 
         Relation expected = rel
                 .restrict(eq(SID, "S1"))
-                .summarize(attrs(SID), count(), attr("count"));
+                .summarize(attrs(SID, PID), count(), attr("count"));
 
         assertSameExpression(expected, optimized);
+    }
 
-        operator =  summarize(rel,attrs(SID), max(QTY),attr("max"));;
-        optimized = optimized(operator).restrict(eq(SID, "S1"));
+    @Test(expected=TypeException.class)
+    public void testRestrict1(){
 
-        expected = rel
-                .restrict(eq(SID, "S1"))
-                .summarize(attrs(SID), count(), attr("max"));
+        // restrict attr is not in the header
+        Relation operator =  summarize(rel,attrs(SID, PID), count(), attr("count"));
+        optimized(operator).restrict(eq(QTY, 400));
 
-        assertSameExpression(expected, optimized);
     }
 
 }
